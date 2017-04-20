8c2e44a24e20e6c71603b92ed98dee1c38684167
Alexander Klimov <alexander.klimov@arcusys.fi>
2017-04-20 17:43:28 +0300

    Add support for sessionID generation, based on author and group info.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-ep_auth_session
+ep_auth_session_extended
 =============
 
 A small script for [etherpad-lite](https://github.com/ether/etherpad-lite) which provides a route that authenticates the user with the provided sessionID parameter and redirects to the given padName.
@@ -9,18 +9,18 @@ Usefull when integrating etherpad in a application on a another domain.
 [![license][license-image]][license-url]
 [![npm download][download-image]][download-url]
 
-[npm-image]: http://img.shields.io/npm/v/ep_auth_session.svg?style=flat-square
-[npm-url]: http://www.npmjs.com/package/ep_auth_session
+[npm-image]: http://img.shields.io/npm/v/ep_auth_session_extended.svg?style=flat-square
+[npm-url]: http://www.npmjs.com/package/ep_auth_session_extended
 [node-image]: https://img.shields.io/badge/node.js-%3E=_0.10-green.svg?style=flat-square
 [node-url]: http://nodejs.org/download/
-[license-image]: https://img.shields.io/npm/l/ep_auth_session.svg?style=flat-square
-[license-url]: https://www.npmjs.com/package/ep_auth_session
-[download-image]: https://img.shields.io/npm/dt/ep_auth_session.svg?style=flat-square
-[download-url]: https://www.npmjs.com/package/ep_auth_session
+[license-image]: https://img.shields.io/npm/l/ep_auth_session_extended.svg?style=flat-square
+[license-url]: https://www.npmjs.com/package/ep_auth_session_extended
+[download-image]: https://img.shields.io/npm/dt/ep_auth_session_extended.svg?style=flat-square
+[download-url]: https://www.npmjs.com/package/ep_auth_session_extended
 
 
 ## Install
-[![ep_auth_session](https://nodei.co/npm/ep_auth_session.png)](https://www.npmjs.com/package/ep_auth_session)
+[![ep_auth_session_extended](https://nodei.co/npm/ep_auth_session_extended.png)](https://www.npmjs.com/package/ep_auth_session_extended)
 
 Restart your etherpad-lite instance to recognize the plugin.
 
--- index.js
+++ index.js
@@ -1,3 +1,114 @@
+var request = require('request'),
+  	Promise = require('promise'),
+  	extend = require('util')._extend;
+
+var apiError = function (message, status, error) {
+  return {
+    name: 'EtherpadError',
+    message: message,
+    status: status || 500,
+    type: 'etherpad',
+    error: error
+  };
+};
+
+var apiConnectionError = function (error) {
+  return apiError('Unable to connect to Etherpad host', 500, error);
+};
+
+var apiRequest = function (options) {
+  var opts = options;
+
+  return new Promise(function(resolve, reject) {
+    request(opts, function (error, response, body) {
+
+      if(error) {
+        reject(apiConnectionError(error));
+        return;
+      }
+
+      var data = body;
+      if (body && typeof body !== 'object') {
+        try {
+          data = JSON.parse(body);
+        } catch (error) {
+          console.log('body is not json object');
+        }
+      }
+      resolve({response: response, data: data});
+    });
+  });
+};
+
+var makeRequest = function(opts) {
+  return new Promise(function(resolve, reject) {
+    apiRequest(opts.options).then(function(result) {
+      if (result.response.statusCode !== 200) {
+        var message = result.data.message || result.data.body.message;
+        reject(apiError(message, result.response.statusCode, result.data));
+        return;
+      }
+
+      var res = opts.dataField ? result.data.data[opts.dataField] : result.data;
+      resolve(res);
+    });
+  });
+};
+
+var getSessionId = function(opts) {
+	var defaultOptions = {method: 'POST', json: true};
+	var body = {api_key: opts.apiKey};
+	var apiPrefix = '/api/' + opts.apiVersion + '/';
+	var apiUrl = opts.protocol + '://' + opts.host + apiPrefix;
+
+	var createAuthor = function() {
+	  return makeRequest({
+      options: extend({
+        url: apiUrl + 'createAuthorIfNotExistsFor',
+        body: extend({
+          authorName: opts.authorName,
+          authorMapper: opts.authorMapper
+        }, body)
+      }, defaultOptions),
+      dataField: 'authorID'
+    });
+  };
+
+	var createGroup = function(authorID) {
+    return new Promise(function(resolve, reject) {
+      makeRequest({
+        options: extend({
+          url: apiUrl + 'createGroupIfNotExistsFor',
+          body: extend({groupMapper: opts.groupMapper}, body)
+        }, defaultOptions),
+        dataField: 'groupID'
+      }).then(function (groupID) {
+        resolve({authorID: authorID, groupID: groupID});
+      });
+    });
+  };
+
+  var createSession = function(res2) {
+    return new Promise(function(resolve, reject) {
+      makeRequest({
+        options: extend({
+          url: apiUrl + 'createSession',
+          body: extend({
+            groupID: res2.groupID,
+            authorID: res2.authorID,
+            validUntil: opts.validUntil
+          }, body)
+        }, defaultOptions),
+        dataField: 'sessionID'
+      }).then(function (sessionID) {
+        resolve(extend({sessionID: sessionID}, res2));
+      });
+    });
+  };
+
+  return createAuthor().then(createGroup).then(createSession);
+};
+
 exports.registerRoute = function(hook_name, args, cb) {
 	args.app.get("/auth_session", function(req, res) {
 		var r = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">' + "\n";
@@ -9,25 +120,52 @@ exports.registerRoute = function(hook_name, args, cb) {
 		r += '<body>' + "\n";
 		r += '<script type="text/javascript">' + "\n";
 
+    var redirectWithSession = function(sessionID, groupID, padName, queryString) {
+      r += 'document.cookie = "sessionID=' + encodeURIComponent(sessionID) + '; path=/;";' + "\n";
+
+      if (padName) {
+        var redirectUrl = '/p/';
+
+        if (groupID) {
+          redirectUrl += encodeURIComponent(groupID) + '$';
+        }
+
+        redirectUrl += encodeURIComponent(padName);
+        redirectUrl += queryString || '';
+        r += 'document.location.href="' + redirectUrl + '";' + "\n";
+      }
+
+      r += '</script>' + "\n";
+      r += '</body>' + "\n";
+      r += '</html>' + "\n";
+
+      res.send(r);
+    };
+
 		if (req.query.sessionID) {
-			r += 'document.cookie = "sessionID=' + encodeURIComponent(req.query.sessionID) + '; path=/;";' + "\n";
-		}
-
-		if (req.query.padName) {
-			var redirectUrl = '/p/';
-
-			if (req.query.groupID) {
-				redirectUrl += encodeURIComponent(req.query.groupID) + '$';
-			}
-
-			redirectUrl += encodeURIComponent(req.query.padName);
-			r += 'document.location.href="' + redirectUrl + '";' + "\n";
-		}
-
-		r += '</script>' + "\n";
-		r += '</body>' + "\n";
-		r += '</html>' + "\n";
-
-		res.send(r);
+		  redirectWithSession(req.query.sessionID, req.query.groupID, req.query.padName);
+		} else if(req.query.apiKey) {
+      getSessionId({
+        host: req.headers.host,
+        protocol: req.protocol,
+        apiKey: req.query.apiKey,
+        apiVersion: '1.2.13',
+        authorName: req.query.authorName,
+        authorMapper: req.query.authorMapper,
+        groupMapper: req.query.groupMapper,
+        validUntil: req.query.validUntil
+      }).then(function (res) {
+        var newQuery = extend({}, req.query);
+        var queryValues = [];
+        ['apiKey', 'authorName', 'authorMapper', 'groupMapper', 'validUntil', 'padName'].forEach(function (key) {
+          delete newQuery[key];
+        });
+        for(var key in newQuery) {
+          queryValues.push(key + '=' + newQuery[key]);
+        }
+        var qs = '?' + queryValues.join('&');
+        redirectWithSession(res.sessionID, res.groupID, req.query.padName, qs);
+      });
+    }
 	});
 };
